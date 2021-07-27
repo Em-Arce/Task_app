3da5d3c739dbc536f3edff9adf6a87b14c74e1e7
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-27 22:46:49 +0800

    add create method and category_params to unable to find Create category button

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,18 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render :new
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
--- spec/features/create_categories_spec.rb
+++ spec/features/create_categories_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "CreateCategories", type: :system do
     #fill in form with required info
     fill_in 'Name', with: 'This is a category'
     #click submit button
-    click_button 'Create Update'
+    click_button 'Create Category'
     #expect page to have the content submitted
     expect(page).to have_content('This is a category')
   end
