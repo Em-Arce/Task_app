2f5b0f9971093671892fbcf090c2318d7a0055fe
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-27 19:43:07 +0800

    Add rspec test for category creation

---
+++ spec/features/create_categories_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "CreateCategories", type: :system
+
+  it 'creates category, saves and shows newly created category' do
+    # visit root route
+    visit '/'
+    #click create category link
+    click_link 'Create Category'
+    #fill in form with required info
+    fill_in 'Name', with: 'This is a category'
+    #click submit button
+    click_button 'Create Update'
+    #expect page to have the content submitted
+    expect(page).to have_content('This is a category')
+  end
+end
