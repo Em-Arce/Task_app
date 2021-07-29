33827856808cbecd3bf77f937f82e9a99a904a14
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-29 15:41:51 +0800

    Add back and delete button tests

--- spec/features/show_category_spec.rb
+++ spec/features/show_category_spec.rb
@@ -5,13 +5,32 @@ RSpec.describe 'Shows A Category', type: :system do
     driven_by(:rack_test)
   end
 
+  it 'when Back is clicked redirects to Root/Index Page' do
+    category = Category.create!(name: 'This is a category')
+    visit category_path(id: category.id)
+    expect(page).to have_content('This is a category')
+    click_link 'Back'
+    visit root_path
+    expect(page).to have_content('Categories#index')
+  end
+
   it 'when Edit is clicked redirects to Edit Page' do
     category = Category.create!(name: 'This is a category')
     visit category_path(id: category.id)
     expect(page).to have_content('This is a category')
     click_link 'Edit'
     visit edit_category_path(id: category.id)
-    expect(page).to have_content('This is a category')
-    expect(page).to have_button('Update Category')
+    expect(page).to have_content('Edit Category')
+    expect(page).to have_content('Name')
+    expect(page).to have_selector("#single_category, div", text: category.name)
   end
+
+  #it 'when Delete is clicked redirects to Edit Page' do
+  #  category = Category.create!(name: 'This is a category')
+  # visit category_path(id: category.id)
+  #  expect(page).to have_content('This is a category')
+  #  click_link 'Delete'
+  #  visit category_path(id: category.id, method: destroy)
+  #  expect(page).to have_content('Are you sure?')
+  #end
 end
