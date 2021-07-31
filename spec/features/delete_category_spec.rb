421334e88fe34008300d332ec486d20cffa408b6
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-31 13:07:37 +0800

    refactor delete specs

--- spec/features/delete_category_spec.rb
+++ spec/features/delete_category_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 RSpec.describe 'Deletes a category', type: :feature do
 
   it 'success' do
-    Category.create!(name: 'This is a category')
-    visit root_path
-    expect(page).to have_content('This is a category')
-    click_link 'Delete'
-    expect(page).not_to have_content('This is a category')
+    category = Category.create!(name: 'Work1')
+    visit categories_path
+    expect(current_path).to eq categories_path
+    expect(page).to have_content('Work1')
+    #use this if there are multiple buttons w/ same name in page else use click_link
+    first(:link, 'Delete').click
+    expect(current_path).to eq categories_path
+    #rspec method to check if record is deleted from db
+    expect { category.destroy }.to change { Category.count }.by(-1)
+    #ActiveRecord methods to check if record is deleted from db
+    expect { category.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    expect(category.persisted?).to be_falsey
   end
 end
