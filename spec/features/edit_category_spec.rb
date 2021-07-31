52ddc772f5ee27a7da0f33c617b7e0a8e89d9dff
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-07-31 13:09:40 +0800

    add expect current_path

--- spec/features/edit_category_spec.rb
+++ spec/features/edit_category_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'EditCategories', type: :feature do
+RSpec.describe 'Edits a category', type: :feature do
 
   it 'edits category, saves and shows edited category' do
     category = Category.create!(name: 'This is a category')
     visit edit_category_path(id: category.id)
+    expect(current_path).to eq edit_category_path(id: category.id)
     fill_in 'Name', with: 'This is a category edited'
+    #click submit button (in form, it is button)
     click_button 'Update Category'
     expect(page).to have_content('This is a category edited')
 
