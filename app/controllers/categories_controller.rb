67833e91b09a949086207e1270d73c6a9492ffb1
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-04 17:50:16 +0800

    Add image_url in category_params to be able to save it in db

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -58,6 +58,6 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :image_url)
   end
 end
