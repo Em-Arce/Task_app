208b38d76e886fb36c022b0873400a0a4c259016
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-05 14:14:16 +0800

    Add test for image_url for valid inputs

--- spec/features/edit_category_spec.rb
+++ spec/features/edit_category_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Edits a category', type: :feature do
 
-  it 'edits category, saves and shows edited category' do
-    category = Category.create!(name: 'This is a category')
+  it 'valid inputs' do
+    category = Category.create!(name: 'Work', image_url:'https://tinyurl.com/2vhaj485')
     visit edit_category_path(id: category.id)
     expect(current_path).to eq edit_category_path(id: category.id)
-    fill_in 'Name', with: 'This is a category edited'
+    fill_in 'Name', with: 'Work edited'
+    fill_in 'Image url', with: 'https://tinyurl.com/sa9k2cep'
     #click submit button (in form, it is button)
     click_button 'Update Category'
-    expect(page).to have_content('This is a category edited')
+    expect(page).to have_content('Work edited')
+    find("img[src='https://tinyurl.com/sa9k2cep']")
 
     cat1 = Category.order("id").last
-    expect(cat1.name).to eq('This is a category edited')
+    expect(cat1.name).to eq('Work edited')
+    expect(cat1.image_url).to eq('https://tinyurl.com/sa9k2cep')
   end
 end
 
