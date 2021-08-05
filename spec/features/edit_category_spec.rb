6493441e24987343cc5e0c4c2809f3fb9d2a73cb
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-05 15:13:57 +0800

    Add test for image_url

--- spec/features/edit_category_spec.rb
+++ spec/features/edit_category_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe 'Edits a category', type: :feature do
     fill_in 'Image url', with: 'https://tinyurl.com/sa9k2cep'
     #click submit button (in form, it is button)
     click_button 'Update Category'
+    visit category_path(id: category.id)
+    expect(current_path).to eq category_path(id: category.id)
     expect(page).to have_content('Work edited')
     find("img[src='https://tinyurl.com/sa9k2cep']")
 
@@ -17,5 +19,16 @@ RSpec.describe 'Edits a category', type: :feature do
     expect(cat1.name).to eq('Work edited')
     expect(cat1.image_url).to eq('https://tinyurl.com/sa9k2cep')
   end
+
+  #it 'for invalid input (Name)' do
+  #  category = Category.create!(name: 'Work', image_url:'https://tinyurl.com/2vhaj485')
+  #  visit edit_category_path(id: category.id)
+  #  expect(current_path).to eq edit_category_path(id: category.id)
+  #  fill_in 'Name', with: ''
+  #  click_button 'Update Category'
+  #  visit edit_category_path(id: category.id)
+  #  expect(current_path).to eq edit_category_path(id: category.id)
+  #  expect(page).to have_content("Name can't be blank")
+  #end
 end
 
