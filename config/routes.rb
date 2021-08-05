b9036bb7c39de715056e67d529a454b2cc8329d0
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-05 15:19:09 +0800

    Added and installed bootstrap and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   get 'categories/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'categories#index'
+  root 'home#index'
   resources :categories
 end
