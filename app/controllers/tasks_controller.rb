06b5314fc9f4f084068f968683146ff4d5290ad7
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-06 20:49:04 +0800

    Add case when task is not saved

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,15 @@ class TasksController < ApplicationController
   def create
     @category = Category.find(params[:category_id])
     @task = @category.tasks.create(task_params)
-    redirect_to category_path(@category)
+    if @task.save
+      #redirect_to category_path(@category)
+      redirect_to @task.category
+    else
+      render :new
+    end
   end
+
+
 
   private
     def task_params
