68f74a7ceebab1e59f70dbe84fb24112c31f4c79
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-07 12:01:36 +0800

    Added associations between user and tasks

--- db/migrate/20210806124751_add_user_id_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTasks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :tasks, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_124751) do
+ActiveRecord::Schema.define(version: 2021_08_07_035849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 2021_08_06_124751) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_08_06_124751) do
   end
 
   add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "users"
 end
