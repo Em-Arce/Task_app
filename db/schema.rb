bfd53f7c411a3fc3eb64ccefbce5fbd9d87adaa5
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-08 11:54:43 +0800

    Add user_id to tasks and categories table

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_035849) do
-
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema.define(version: 2021_08_07_080258) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
@@ -20,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_08_07_035849) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "image_url"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -48,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_08_07_035849) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "tasks", "categories"
   add_foreign_key "tasks", "users"
 end
