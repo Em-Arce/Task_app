1ff56691b0ca3516b96e447bc0f655d582db56b9
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-08 13:21:01 +0800

    Fix the set_category, uncomment before_action correct user and remove unnecessary codes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,29 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  #before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  #before_action :set_category, only: %i[create]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: %i[create]
   before_action :set_task, only: %i[show edit update destroy]
 
   def new
     @task = @category.tasks.build
-    #@task = current_user.tasks.build
   end
 
   def show; end
 
   def create
-    @category = Category.find(params[:category_id])
     @task = @category.tasks.create(task_params)
+    @task.user_id = current_user.id
     if @task.save
       redirect_to @task.category
-      #@task.category
     else
       render :new
     end
   end
 
   private
-
+    def set_category
+      @category = Category.find(params[:category_id])
+    end
 
     def set_task
       @task = Task.find(params[:id])
