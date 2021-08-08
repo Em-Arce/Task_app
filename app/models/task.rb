faa86b2bf243a67058bb46bd02f0dc67c12c5fa8
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-08 11:49:07 +0800

    Add association with categories

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :priority, presence: true
+  validates :deadline, presence: true
+  validates :completed, inclusion: { in: [true,false] }
 end
