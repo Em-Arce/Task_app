5e2bad21b0a7f221c0d80f6334dc14144b9de0c6
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-08 15:12:07 +0800

    Add test for invalid input fields

--- spec/features/edit_category_spec.rb
+++ spec/features/edit_category_spec.rb
@@ -33,15 +33,18 @@ RSpec.describe 'Edits a category', type: :feature do
     expect(cat1.image_url).to eq('https://tinyurl.com/sa9k2cep')
   end
 
-  #it 'for invalid input (Name)' do
-  #  category = Category.create!(name: 'Work', image_url:'https://tinyurl.com/2vhaj485')
-  #  visit edit_category_path(id: category.id)
-  #  expect(current_path).to eq edit_category_path(id: category.id)
-  #  fill_in 'Name', with: ''
-  #  click_button 'Update Category'
-  #  visit edit_category_path(id: category.id)
-  #  expect(current_path).to eq edit_category_path(id: category.id)
-  #  expect(page).to have_content("Name can't be blank")
-  #end
+  it 'for invalid input (Name)' do
+    expect(current_path).to eq edit_category_path(id: category.id)
+    fill_in 'Name', with: ''
+    click_button 'Update Category'
+    expect(page).to have_content("Name can't be blank")
+  end
+
+  it 'for invalid input (Image url)' do
+    expect(current_path).to eq edit_category_path(id: category.id)
+    fill_in 'Image url', with: ''
+    click_button 'Update Category'
+    expect(page).to have_content("Image url can't be blank")
+  end
 end
 
