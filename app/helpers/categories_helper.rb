b9a25fa4dae26fe469631c9b4c36568ba5433935
Bon Axl Feeser <bonaxl015@gmail.com>
2021-08-10 14:26:21 +0800

    Added a method to translate priority into high, medium or low

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -8,6 +8,17 @@ module CategoriesHelper
     def percent_progress(category_id)
         @task_completed = Task.where(category_id: category_id, completed: true).count
         @total_task = Task.where(category_id: category_id).count
-        @total_task > 0 ? @percent_progress = ((@task_completed / @total_task) * 100).round(2) : @percent_progress = 0
+        @total_task > 0 ? @percent_progress = ((@task_completed / @total_task.to_f) * 100).round(2) : @percent_progress = 0
+    end
+
+    def translate_priority(priority)
+        case priority
+        when 1
+            "High"
+        when 2
+            "Medium"
+        when 3
+            "Low"
+        end
     end
 end
