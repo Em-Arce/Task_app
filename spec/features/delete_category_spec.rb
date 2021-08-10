8751104969b1f4d86f8fce427f93470ac1529695
Em Jen P Arce <ejparce@IEmAwesome.localdomain>
2021-08-10 15:24:50 +0800

    Refactor and document what happened why it failed

--- spec/features/delete_category_spec.rb
+++ spec/features/delete_category_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Deletes a category', type: :feature do
   let(:user) {User.create!(email: 'test@example.com', password: 'password')}
-  let(:category) {Category.create!(name: 'Work', image_url:'https://tinyurl.com/2vhaj485', user: user)}
+  let (:category) { Category.create(name: 'This is a category',
+                      image_url:'https://tinyurl.com/2vhaj485', user: user) }
 
   def login(user)
     visit home_index_path
@@ -14,17 +15,21 @@ RSpec.describe 'Deletes a category', type: :feature do
 
   before do
     login(user)
-    visit categories_path
+    visit category_path(category)
   end
 
   it 'success' do
-    expect(current_path).to eq categories_path
+    expect(current_path).to eq category_path(category)
+    category_count = Category.count
     #use this if there are multiple buttons w/ same name in page else use click_link
     #first(:link, 'Delete').click
-    category_count = Category.count
+    #after git merge 09 Aug, this did not work anymore: removed
+    #Delete button in show category page and retained in index but testing
+    #delete from index makes below commands return capybara error
     click_link 'Delete'
+    #find("#delete").click
+    #find("a[href='/categories/#{category.id}']").click
     expect(Category.count).to eq(category_count - 1)
-    expect(current_path).to eq categories_path
 
     #ActiveRecord methods to check if exact record is deleted from db
     expect { category.reload }.to raise_error(ActiveRecord::RecordNotFound)
